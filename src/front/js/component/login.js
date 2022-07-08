0afeb9601cd206a660ac276b34fe49f007c1bf2a
INVIiICTUS <j.enriquealorenzo@gmail.com>
2022-07-08 18:42:22 +0000

    a medias

---
+++ src/front/js/component/login.js
@@ -0,0 +1,43 @@
+import { Action } from "history";
+import React, { useState, useContext } from "react";
+import ReactDOM from "react-dom";
+import { Context } from "../store/appContext";
+
+export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { store, actions } = useContext(Context);
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    actions.login(email, password);
+  };
+  return (
+    <div className="container justify-content-center w-25 rounded-3 bg-light ">
+      <div className="form">
+        <form onSubmit={handlesubmit}>
+          <div className="input-container ">
+            <label> E - mail </label>{" "}
+            <input
+              type="text"
+              name="uname"
+              required
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>{" "}
+          <div className="input-container">
+            <label> Password </label>{" "}
+            <input
+              type="password"
+              name="pass"
+              required
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>{" "}
+          <div className="button-container">
+            <input type="submit" />
+          </div>{" "}
+        </form>
+      </div>{" "}
+    </div>
+  );
+};
