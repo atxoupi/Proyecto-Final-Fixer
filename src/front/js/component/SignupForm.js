307dcf15c22e42bdf4c853d1fbeef155c5d83b07
Aleisgar <aleisgar@gmail.com>
2022-07-09 19:10:33 +0000

    forms finish

--- src/front/js/component/SignupForm.js
+++ src/front/js/component/SignupForm.js
@@ -1,155 +1,259 @@
 import React from "react";
 
+
 export const SignupForm = () => {
   return (
-    <div className="container py-5 h-100">
-      <div className="row justify-content-center align-items-center h-100">
-        <div className="col-12 col-lg-9 col-xl-7">
-          <div
-            className="card shadow-2-strong card-registration"
-            style={{borderRadius:"15px"}}
+    <>
+      {/* Nav bar con botones usuario/profesional */}
+      <ul
+        class="nav nav-pills mb-3 mt-3 d-flex justify-content-center"
+        id="pills-tab"
+        role="tablist"
+      >
+        <li class="nav-item" role="presentation">
+          <button
+            class="nav-link active"
+            id="pills-home-tab"
+            data-bs-toggle="pill"
+            data-bs-target="#pills-user"
+            type="button"
+            role="tab"
+            aria-controls="pills-home"
+            aria-selected="true"
           >
-            <div className="card-body p-4 p-md-5">
-              <h3 className="mb-4 pb-2 pb-md-0 mb-md-5">Registration Form</h3>
-              <form>
-                <div className="row">
-                  <div className="col-md-6 mb-4">
-                    <div className="form-outline">
-                      <input
-                        type="text"
-                        id="firstName"
-                        className="form-control form-control-lg"
-                      />
-                      <label className="form-label" htmlFor="firstName">
-                        First Name
-                      </label>
-                    </div>
-                  </div>
-                  <div className="col-md-6 mb-4">
-                    <div className="form-outline">
-                      <input
-                        type="text"
-                        id="lastName"
-                        className="form-control form-control-lg"
-                      />
-                      <label className="form-label" htmlFor="lastName">
-                        Last Name
-                      </label>
-                    </div>
-                  </div>
-                </div>
+            Usuario
+          </button>
+        </li>
+        <li class="nav-item" role="presentation">
+          <button
+            class="nav-link"
+            id="pills-profile-tab"
+            data-bs-toggle="pill"
+            data-bs-target="#pills-worker"
+            type="button"
+            role="tab"
+            aria-controls="pills-profile"
+            aria-selected="false"
+          >
+            Profesional
+          </button>
+        </li>
+      </ul>
 
-                <div className="row">
-                  <div className="col-md-6 mb-4 d-flex align-items-center">
-                    <div className="form-outline datepicker w-100">
-                      <input
-                        type="text"
-                        className="form-control form-control-lg"
-                        id="birthdayDate"
-                      />
-                      <label htmlFor="birthdayDate" className="form-label">
-                        Birthday
-                      </label>
-                    </div>
-                  </div>
-                  <div className="col-md-6 mb-4">
-                    <h6 className="mb-2 pb-1">Gender: </h6>
+      {/* Formulario de usuario */}
 
-                    <div className="form-check form-check-inline">
-                      <input
-                        className="form-check-input"
-                        type="radio"
-                        name="inlineRadioOptions"
-                        id="femaleGender"
-                        value="option1"
-                        
-                      />
-                      <label className="form-check-label" htmlFor="femaleGender">
-                        Female
-                      </label>
-                    </div>
+      <div class="tab-content" id="pills-tabContent">
+        <div
+          class="tab-pane fade show active"
+          id="pills-user"
+          role="tabpanel"
+          aria-labelledby="pills-home-tab"
+        >
+          <div className="container h-60">
+            <div className="row justify-content-center align-items-center h-100">
+              <div className="col-12 col-lg-9 col-xl-7">
+                <div
+                  className="card shadow-2-strong card-registration"
+                  style={{ borderRadius: "15px" }}
+                >
+                  <div className="card-body p-4 p-md-5">
+                    <h3 className="mb-4 pb-2 pb-md-0 mb-md-5 text-center">
+                      Para registrarte, rellena los siguientes campos
+                    </h3>
+                    <form>
+                      <div className="row">
+                        <div className="col-md-6 mb-4">
+                          <div className="form-outline">
+                            <input
+                              type="text"
+                              id="firstName"
+                              className="form-control form-control-lg"
+                              placeholder="Nombre"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-md-6 mb-4">
+                          <div className="form-outline">
+                            <input
+                              type="text"
+                              id="lastName"
+                              className="form-control form-control-lg"
+                              placeholder="Apellidos"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                      <div className="row">
+                        <div className="col-md-6 mb-4 pb-2">
+                          <div className="form-outline">
+                            <input
+                              type="email"
+                              id="emailAddress"
+                              className="form-control form-control-lg"
+                              placeholder="email@email.com"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-md-6 mb-4 pb-2">
+                          <div className="form-outline">
+                            <input
+                              type="password"
+                              id="examplePassword1"
+                              className="form-control form-control-lg"
+                              placeholder="password"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                      <div class="mb-3 form-check">
+                        <input
+                          type="checkbox"
+                          class="form-check-input"
+                          id="exampleCheck1"
+                        />
+                        <label
+                          clasName="form-check-label"
+                          htmlFor="exampleCheck1"
+                        >
+                          He leído y acepto la política de privacidad bla
+                          blablbalblalba
+                        </label>
+                      </div>
 
-                    <div className="form-check form-check-inline">
-                      <input
-                        className="form-check-input"
-                        type="radio"
-                        name="inlineRadioOptions"
-                        id="maleGender"
-                        value="option2"
-                      />
-                      <label className="form-check-label" htmlFor="maleGender">
-                        Male
-                      </label>
-                    </div>
-
-                    <div className="form-check form-check-inline">
-                      <input
-                        className="form-check-input"
-                        type="radio"
-                        name="inlineRadioOptions"
-                        id="otherGender"
-                        value="option3"
-                      />
-                      <label className="form-check-label" htmlFor="otherGender">
-                        Other
-                      </label>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="row">
-                  <div className="col-md-6 mb-4 pb-2">
-                    <div className="form-outline">
-                      <input
-                        type="email"
-                        id="emailAddress"
-                        className="form-control form-control-lg"
-                      />
-                      <label className="form-label" htmlFor="emailAddress">
-                        Email
-                      </label>
-                    </div>
-                  </div>
-                  <div className="col-md-6 mb-4 pb-2">
-                    <div className="form-outline">
-                      <input
-                        type="tel"
-                        id="phoneNumber"
-                        className="form-control form-control-lg"
-                      />
-                      <label className="form-label" htmlFor="phoneNumber">
-                        Phone Number
-                      </label>
-                    </div>
+                      <div className="mt-4 pt-2">
+                        <input
+                          className="btn btn-primary btn-lg"
+                          type="submit"
+                          value="Submit"
+                        />
+                      </div>
+                    </form>
                   </div>
                 </div>
+              </div>
+            </div>
+          </div>
+        </div>
 
-                <div className="row">
-                  <div className="col-12">
-                    <select className="select form-control-lg">
-                      <option value="1" disabled>
-                        Choose option
-                      </option>
-                      <option value="2">Subject 1</option>
-                      <option value="3">Subject 2</option>
-                      <option value="4">Subject 3</option>
-                    </select>
-                    <label className="form-label select-label">Choose option</label>
+        {/* Formulario de trabajador */}
+        <div
+          class="tab-pane fade"
+          id="pills-worker"
+          role="tabpanel"
+          aria-labelledby="pills-profile-tab"
+        >
+          <div className="container h-60">
+            <div className="row justify-content-center align-items-center h-100">
+              <div className="col-12 col-lg-9 col-xl-7">
+                <div
+                  className="card shadow-2-strong card-registration"
+                  style={{ borderRadius: "15px" }}
+                >
+                  <div className="card-body p-4 p-md-5">
+                    <h3 className="mb-4 pb-2 pb-md-0 mb-md-5 text-center">
+                      Para registrarte, rellena los siguientes campos
+                    </h3>
+                    <form>
+                      <div className="row">
+                        <div className="col-md-6 mb-4">
+                          <div className="form-outline">
+                            <input
+                              type="text"
+                              id="firstName"
+                              className="form-control form-control-lg"
+                              placeholder="Nombre"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-md-6 mb-4">
+                          <div className="form-outline">
+                            <input
+                              type="text"
+                              id="lastName"
+                              className="form-control form-control-lg"
+                              placeholder="Apellidos"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                      <div className="row">
+                        <div className="col-md-6 mb-4">
+                          <div className="form-outline">
+                            <input
+                              type="text"
+                              id="lastName"
+                              className="form-control form-control-lg"
+                              placeholder="Ciudad o población"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-md-6 mb-4 pb-2">
+                           <label className="form-label select-label">
+                            Selecciona tu sector
+                          </label> <select className="select form-control-lg" multiple aria-label="multiple select example">
+                          
+                            <option value="1">Fontanería</option>
+                            <option value="2">Mudanzas</option>
+                            <option value="3">Carpintería</option>
+                            <option value="4">Pintura</option>
+                            <option value="4">Electricidad</option>
+                            <option value="4">Climatización</option>
+                          </select>
+                          
+                        </div>
+                      </div>
+                      <div className="row">
+                        <div className="col-md-6 mb-4 pb-2">
+                          <div className="form-outline">
+                            <input
+                              type="email"
+                              id="emailAddress"
+                              className="form-control form-control-lg"
+                              placeholder="email@email.com"
+                            />
+                          </div>
+                        </div>
+                        <div className="col-md-6 mb-4 pb-2">
+                          <div className="form-outline">
+                            <input
+                              type="password"
+                              id="examplePassword1"
+                              className="form-control form-control-lg"
+                              placeholder="password"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                      <div class="mb-3 form-check">
+                        <input
+                          type="checkbox"
+                          class="form-check-input"
+                          id="exampleCheck1"
+                        />
+                        <label
+                          clasName="form-check-label"
+                          htmlFor="exampleCheck1"
+                        >
+                          He leído y acepto la política de privacidad bla
+                          blablbalblalba
+                        </label>
+                      </div>
+
+                      <div className="mt-4 pt-2">
+                        <input
+                          className="btn btn-primary btn-lg"
+                          type="submit"
+                          value="Submit"
+                        />
+                      </div>
+                    </form>
                   </div>
                 </div>
-
-                <div className="mt-4 pt-2">
-                  <input
-                    className="btn btn-primary btn-lg"
-                    type="submit"
-                    value="Submit"
-                  />
-                </div>
-              </form>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
