ae47aea86db369efdd9b82990e12fff38ac94056
Aleisgar <aleisgar@gmail.com>
2022-07-16 17:26:01 +0000

    new css

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logoFixer from "../../img/logo-fixer.png";
-import "../../styles/index.css";
+import "../../styles/navbar.css";
 
 export const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg fs-6">
+    <nav className="navbar navbar-expand-lg bg-white fs-6">
       <div className="container-fluid">
         <Link to="/" className="navbar-brand">
           <img
@@ -27,22 +27,22 @@ export const Navbar = () => {
         </button>
       </div>
       <div
-        className="collapse  navbar-collapse w-75"
+        className="collapse navbar-collapse-width "
         id="navbarSupportedContent"
       >
         <ul className="navbar-nav mb-2 mb-lg-0">
           <li className="nav-item mx-1">
-            <Link className="nav-link_navbar " href="#" to="/request">
+            <Link className="nav-link-navbar " href="#" to="/request">
               Publica una oferta
             </Link>
           </li>
           <li className="nav-item mx-1">
-            <Link to="/signup" className="nav-link_navbar w-100" href="#">
+            <Link to="/signup" className="nav-link-navbar w-100" href="#">
               Regístrate
             </Link>
           </li>
           <li className="nav-item mx-1">
-            <Link className="nav-link_navbar" aria-current="page" to="/login">
+            <Link className="nav-link-navbar" aria-current="page" to="/login">
               Accede
             </Link>
           </li>
