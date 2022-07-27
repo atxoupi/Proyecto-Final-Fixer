15551374ac2c61d81c6136f246b7e5e3f4da601c
Aleisgar <aleisgar@gmail.com>
2022-07-27 19:40:25 +0000

    vista budgets acabada

--- src/front/js/pages/postedWorks.js
+++ src/front/js/pages/postedWorks.js
@@ -8,42 +8,44 @@ export const PostedWorks = () => {
   useEffect(() => {
     actions.showWork();
   }, []);
-  console.log(store.work);
-  console.log(store.usuario);
-  console.log(store.icons);
+
   return (
     <>
       {store.usuario ? (
         <div className="container-works mx-auto">
           <h3 className="text-center mb-3">Estas son tus ofertas publicadas</h3>
-          {store.work.map((item, index) => (
-            <WorkPost
-              key={index}
-              description={item.description}
-              location={item.location}
-              sector={item.sector}
-              title={item.title}
-              user_id={item.user_id}
-              worker_id={item.worker_id}
-              work_id={item.id}
-              showButton={true}
-            />
-          ))}
+          {store.work.length > 0
+            ? store.work.map((item, index) => (
+                <WorkPost
+                  key={index}
+                  description={item.description}
+                  location={item.location}
+                  sector={item.sector}
+                  title={item.title}
+                  user_id={item.user_id}
+                  worker_id={item.worker_id}
+                  work_id={item.id}
+                  showButton={true}
+                />
+              ))
+            : null}
         </div>
       ) : (
         <div className="container-works mx-auto">
-          <h3 className="text-center mb-3">Estas las ofertas en tu sector</h3>
-          {store.work.map((item, index) => (
-            <WorkPostForWorker
-              key={index}
-              description={item.description}
-              location={item.location}
-              sector={item.sector}
-              user_id={item.user_id}
-              worker_id={item.worker_id}
-              work_id={item.id}
-            />
-          ))}
+          <h3 className="text-center mb-3"> Estas las ofertas en tu sector </h3>
+          {store.work.length > 0
+            ? store.work.map((item, index) => (
+                <WorkPostForWorker
+                  key={index}
+                  description={item.description}
+                  location={item.location}
+                  sector={item.sector}
+                  user_id={item.user_id}
+                  worker_id={item.worker_id}
+                  work_id={item.id}
+                />
+              ))
+            : null}
         </div>
       )}
     </>
