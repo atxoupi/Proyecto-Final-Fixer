5eaa01851bfe3f56cf0241c2dc086f20b0a131a6
atxoupi <andres.hermelo@gmail.com>
2022-09-05 11:58:34 +0000

    Se cierra el PopUp

--- src/front/js/component/login.js
+++ src/front/js/component/login.js
@@ -3,7 +3,7 @@ import { Navigate } from "react-router-dom";
 
 import { Context } from "../store/appContext";
 import { auth, provider } from "../firebase";
-
+import { signInWithPopup } from "firebase/auth";
 export const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -15,7 +15,25 @@ export const Login = () => {
   };
 
   const googlesignup = () => {
-    auth.signInWithPopup(provider);
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
   };
 
   return (
--- src/front/js/firebase.js
+++ src/front/js/firebase.js
@@ -1,23 +1,19 @@
-import {
-    initializeApp,
-    auth
-} from "firebase/app ";
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAMKVJBMKH6HSrPBLFw92uyDzvRdYgNE3k",
-    authDomain: "fixerlogin-6e7c7.firebaseapp.com",
-    projectId: "fixerlogin-6e7c7",
-    storageBucket: "fixerlogin-6e7c7.appspot.com",
-    messagingSenderId: "852246192558",
-    appId: "1:852246192558:web:c1c79faf2ce0835957a83f",
+  apiKey: "AIzaSyAMKVJBMKH6HSrPBLFw92uyDzvRdYgNE3k",
+  authDomain: "fixerlogin-6e7c7.firebaseapp.com",
+  projectId: "fixerlogin-6e7c7",
+  storageBucket: "fixerlogin-6e7c7.appspot.com",
+  messagingSenderId: "852246192558",
+  appId: "1:852246192558:web:c1c79faf2ce0835957a83f",
 };
 
-const firebaseApp = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-const auth = firebase.auth();
-const provider = firebase.auth.GoogleAuthProvider();
+const auth = getAuth();
 
-export {
-    auth,
-    provider
-};
+const provider = new GoogleAuthProvider();
+
+export { auth, provider };
